46da88e438112e7fc615948c4dc086820652d5a4
itsMontoya <josh@montoya.io>
2016-09-10 16:41:30 -0700

    Adding more comments to helloWorld files

--- cmd/helloWorld/build.sh
+++ cmd/helloWorld/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-go build
-sudo setcap 'cap_net_bind_service=+ep' helloWorld
+go build # Build main.go 
+sudo setcap 'cap_net_bind_service=+ep' helloWorld # Allow helloWorld binary to listen to port(s) 80 and/or 443
--- cmd/helloWorld/main.go
+++ cmd/helloWorld/main.go
@@ -6,9 +6,9 @@ import (
 	"github.com/itsmontoya/webWorkers"
 )
 
-const (
-	cfgLoc = "./config.ini"
-)
+// cfgLoc is the location of our config ini file
+// Note: An example config is provided at ./config.ini.example
+const cfgLoc = "./config.ini"
 
 func main() {
 	var (
@@ -17,21 +17,26 @@ func main() {
 		err error
 	)
 
+	// Get a set of webWorkers options using our configuration file location
 	if o, err = webWorkers.NewOpts(cfgLoc); err != nil {
 		panic(err)
 	}
 
+	// Request a new instance of webWorkers with our options
 	if ww, err = webWorkers.New(o, Handle); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("About to listen")
-	fmt.Println("Err?", ww.Listen())
+	fmt.Println("About to listen at:", o.Address)
+	fmt.Println("Error listening", ww.Listen())
 }
 
 // Handle likes to handle
 func Handle(res *webWorkers.Response, req *webWorkers.Request) {
+	// Set status code to 200 (Status OK)
 	res.StatusCode(200)
+	// Set our content type to application/json
 	res.ContentType("application/json")
+	// Return a static []byteslice of a JSON object
 	res.Write([]byte(`{ "greeting" : "Hello world!" }`))
 }
